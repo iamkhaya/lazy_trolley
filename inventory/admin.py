d74b22c625ceded27c373256b2e51ad1fc03f25a
Khayelihle Tshuma <khayelihle.tshuma@gmail.com>
2017-07-08 14:28:43 +0200

    Add backend crud operations for the backend

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+from .models import ShopBranch
+from .models import ShopCatalogue
+from .models import ShopHeadquaters
+from .models import Category
+from .models import Brand
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brand', 'category', 'description', 'image_1']
+
+class ShopBranchAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent_company', 'city', 'telephone']
+
+class ShopHeadquatersAdmin(admin.ModelAdmin):
+    list_display = ['company_name', 'city', 'region', 'telephone']
+
+class ShopCatalogueAdmin(admin.ModelAdmin):
+    list_display = ['shop_id', 'product_id', 'price']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ShopBranch, ShopBranchAdmin)
+admin.site.register(ShopHeadquaters, ShopHeadquatersAdmin)
+admin.site.register(ShopCatalogue, ShopCatalogueAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -2,13 +2,27 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Product(models.Model):
-    product_id = models.IntegerField()
-    brand = models.CharField(max_length=200)
-    category = models.CharField(max_length=200)
+    """Model for products displayed on site"""
+    class Meta:
+        """pluralization"""
+        verbose_name_plural = "products"
+
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        to_field='name',
+    )
+    brand = models.ForeignKey(
+        'Brand',
+        on_delete=models.CASCADE,
+        to_field='name',
+    )
+
+    barcode = models.IntegerField()
     size = models.IntegerField()
     size_units = models.CharField(max_length=200)
     quantity = models.IntegerField()
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     description = models.TextField()
     image_1 = models.FileField(upload_to='images/products', default='images/products/image_1.jpg')
     image_2 = models.FileField(upload_to='images/products', default='images/products/image_2.jpg')
@@ -18,17 +32,30 @@ class Product(models.Model):
     date_created = models.DateField(auto_now=True)
     created_by = models.CharField(max_length=200, default='admin')
 
+    def __unicode__(self):
+        return u'{0}'.format(self.name)
 
-class ShopBranches(models.Model):
+
+class ShopBranch(models.Model):
+    """Model for shop branches"""
+    class Meta:
+        """pluralization"""
+        verbose_name_plural = "shop Branches"
+
+    parent_company = models.ForeignKey(
+        'ShopHeadquaters',
+        on_delete=models.CASCADE,
+        to_field='company_name',
+    )
+
     shop_id = models.IntegerField()
-    name = models.CharField(max_length=200)
-    parent_company = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     street_number = models.IntegerField()
-    street1 =  models.CharField(max_length=200)
-    street2 =  models.CharField(max_length=200)
+    street1 = models.CharField(max_length=200)
+    street2 = models.CharField(max_length=200)
     suburb = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
-    region =  models.CharField(max_length=200)
+    region = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
     telephone = models.IntegerField()
     website = models.URLField(max_length=200)
@@ -39,32 +66,81 @@ class ShopBranches(models.Model):
     logo = models.FileField(upload_to='images/logos/', default='images/products/logo.jpg')
     created_by = models.CharField(max_length=200, default='admin')
 
+    def __unicode__(self):
+        return u'{0}, {1}'.format(self.parent_company, self.name)
+
 
 class ShopHeadquaters(models.Model):
-    shop_headquaters_id = models.IntegerField()
-    company_name = models.CharField(max_length=200)
+    """Model for shop headquaters"""
+    class Meta:
+        """pluralization"""
+        verbose_name_plural = "shop Headquaters"
+
+    company_name = models.CharField(max_length=200, unique=True)
     street_number = models.IntegerField()
-    street1 =  models.CharField(max_length=200)
-    street2 =  models.CharField(max_length=200)
+    street1 = models.CharField(max_length=200)
+    street2 = models.CharField(max_length=200)
     suburb = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
-    region =  models.CharField(max_length=200)
-    country = models.CharField(max_length=200)
+    region = models.CharField(max_length=200)
+    country = models.CharField(max_length=200, default='Zimbabwe')
     telephone = models.IntegerField()
     website = models.URLField(max_length=200)
     date_created = models.DateField(auto_now=True)
     created_by = models.CharField(max_length=200, default='admin')
 
+    def __unicode__(self):
+        return u'{0}'.format(self.company_name)
+
 class ShopCatalogue(models.Model):
-    shop_id = models.IntegerField()
-    product_id = models.IntegerField()
+    """Model for Shop Catalogue"""
+    class Meta:
+        """pluralization"""
+        verbose_name_plural = "shop Catalogues"
+
+    shop = models.ForeignKey(
+        'ShopBranch',
+        on_delete=models.CASCADE,
+        to_field='name',
+    )
+
+    product = models.ForeignKey(
+        'Product',
+        on_delete=models.CASCADE,
+        to_field='name',
+    )
+
     price = models.DecimalField(max_digits=7, decimal_places=2)
+    special_price = models.DecimalField(max_digits=7, decimal_places=2)
     date_created = models.DateField(auto_now=True)
     created_by = models.CharField(max_length=200, default='admin')
 
+
 class Category(models.Model):
-    category_id = models.IntegerField()
-    name = models.CharField(max_length=200)
+    """Model forcategories"""
+    class Meta:
+        """pluralization"""
+        verbose_name_plural = "categories"
+
+    name = models.CharField(max_length=200, unique=True)
     description = models.CharField(max_length=200)
     date_created = models.DateField(auto_now=True)
     created_by = models.CharField(max_length=200, default='admin')
+
+    def __unicode__(self):
+        return u'{0}'.format(self.name)
+
+
+class Brand(models.Model):
+    """Model for Brands"""
+    class Meta:
+        """pluralization"""
+        verbose_name_plural = "brands"
+
+    name = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=200)
+    date_created = models.DateField(auto_now=True)
+    created_by = models.CharField(max_length=200, default='admin')
+
+    def __unicode__(self):
+        return u'{0}'.format(self.name)
